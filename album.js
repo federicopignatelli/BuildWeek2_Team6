3f88b6600744b42f6d144ef2b7606166f5f7d17c
Eros Savogin <eros.sav@gmail.com>
2023-10-16 22:24:33 +0200

    modifiche
    🎃

--- album.js
+++ album.js
@@ -1,7 +1,7 @@
 const navbar = document.getElementById("navbar")
 const nameAlbum = document.getElementById("nameAlbum")
 const cardContainer = document.getElementById("card-container")
-
+const albumNavbar = document.createElement("span")
 
 
 
@@ -9,30 +9,38 @@ const cardContainer = document.getElementById("card-container")
 // const albumId = addressBarContent.get("albumId")
 const albumId = "75621062"
 
-const songs = (obj1) => {
-
-    // const albumNavbar = `<span class="position-relative" style="left:35vw" id="nameAlbum">Album</span>`
-    // navbar.appendChild(albumNavbar)
+const header = (obj1) => {
 
     const header = document.getElementById("header")
 
     header.innerHTML = `
     <div class="d-flex justify-content-center">
-    <img src="${obj1.cover_medium}" alt="album-image" class="w-50" id="main-album-img">
-</div>
-
-<div class="my-2">
-
+    <img src="${obj1.cover_medium}" alt="album-image" class="w-50 mt-3" id="main-album-img">
+    </div>
+    
+    <div class="my-2">
+    
     <h2 class="mt-4">${obj1.title}</h2>
-
+    
     <div class="">
         <img class="rounded-circle" style="width:30px" src="${obj1.artist.picture_small}" alt="artist-icon">
-        <span class="ms-1">${obj1.artist.name}</span>
-        <p class="my-3" style="font-size:14px">Album · <span id="anno-album">${obj1.release_date}</span></p>
+        <a class="btn border-0 text-light ps-1 fw-bold" style="font-size:14px">${obj1.artist.name}</a>
+        <p class="my-3" style="font-size:12px">Album · <span id="anno-album">${obj1.release_date.slice(0,4)}</span></p>
     </div>
+    
+    </div>
+    `
+}
 
-</div>
-`
+const songs = (obj1) => {
+
+    albumNavbar.innerHTML = `${obj1.title}`
+    albumNavbar.style.fontSize = "12px"
+    albumNavbar.style.opacity = "0"
+    albumNavbar.classList.add("ms-3")
+    navbar.appendChild(albumNavbar)
+
+    header(obj1)
 
     obj1.tracks.data.forEach((objData) => {
 
@@ -40,8 +48,8 @@ const songs = (obj1) => {
         cardSong.classList.add("d-flex", "justify-content-between")
         cardSong.innerHTML = `
         <div>
-        <h5 class="fw-bold">${objData.title}</h5>
-        <p>${objData.artist.name}</p>
+        <a class="btn text-start p-0 text-light border-0" href=""><h6 class="fw-bold mb-0">${objData.title}</h6></a>
+        <p class="" style="font-size:12px">${objData.artist.name}</p>
 
         </div>
 
@@ -82,11 +90,11 @@ fetch("https://striveschool-api.herokuapp.com/api/deezer/album/" + albumId)
 window.addEventListener("scroll", () => {
     if (window.scrollY > 160) {
         navbar.style.backgroundColor = "rgb(156, 111, 34)"
-        navbar.style.boxShadow = "5px 5px 20px black"
-        nameAlbum.style.opacity = "1"
+        navbar.style.boxShadow = "0px 0px 60px black"
+        albumNavbar.style.opacity = "1"
     } else {
         navbar.style.boxShadow = ""
         navbar.style.backgroundColor = "rgba(156, 111, 34, 0)"
-        nameAlbum.style.opacity = "0"
+        albumNavbar.style.opacity = "0"
     }
 })
