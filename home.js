e03c895118d4d38edb40dea88a75e8db3f064a61
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-10-17 09:37:30 +0200

    md style + js

--- home.js
+++ home.js
@@ -1,1 +1,22 @@
-// sidebar
+const nameBande = 
+
+
+const getSearchHome = async function () {
+    try {
+        const response = await fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=')
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data)
+        } else {
+            throw new Error('Errore nel contattare il server')
+        }
+    } catch (error) {
+        console.log('Si è verificato un errore:', error)
+    }
+}
+
+getSearchHome()
+
+const renderAlbumHome = function () {
+
+}
