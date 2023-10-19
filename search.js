e2ac76a650002832bbea078362d6d18e6c4982e7
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-10-19 16:39:49 +0200

    js

--- search.js
+++ search.js
@@ -1,4 +1,15 @@
 
+
+const sidebar = document.getElementById("side-bar");
+const middle = document.getElementById("col-middle");
+  
+const funz = function () {
+    sidebar.classList.toggle("d-md-none");
+    middle.classList.toggle("col-lg-9");
+};
+
+
+
 const address = new URLSearchParams(location.search)
 // const formulazione = addressBarContent.get('searchId')
 
@@ -24,7 +35,7 @@ const search=function (query) {
 
 
       
-   
+       cardSearch(data)
 
      
         const row=document.getElementById("contenitore")
@@ -60,32 +71,33 @@ const search=function (query) {
 }
 
 
-const cardSearch=function(data) {
-  const row=document.getElementById("contenitore")
-row.innerHTML=""
-for (let i = 0; i < 25; i++) {
-const col = document.createElement("div");
-col.classList.add("col-6", "col-lg-3", "col-md-4");
+// const cardSearch=function(data) {
+//   const row=document.getElementById("contenitore")
+//   row.innerHTML=""
+// for (let i = 0; i < data.length; i++) {
+//  const col = document.createElement("div");
+//  col.classList.add("col-6", "col-lg-3", "col-md-4");
 
-const randomNum = Math.round(Math.random() * 25);
-col.innerHTML = `<div class="card bg-black text-white my-2">
-<img
- src="${data.data[randomNum].artist.picture_medium}"
- class="card-img-top  p-3"
- alt="..."
-/>
-<div class="card-body">
- <h5 class="card-title text-truncate overflow-hidden text-nowrap">${data.data[randomNum].album.title}</h5>
- <p class="card-text text-truncate overflow-hidden">${data.data[randomNum].artist.name}</p>
-</div>
-</div>;` 
-row.appendChild(col);
-}
-}
+//  const randomNum = Math.round(Math.random() * 25);
+//  col.innerHTML = `<div class="card bg-black text-white my-2">
+//  <img
+//  src="${data.data[randomNum].artist.picture_medium}"
+//  class="card-img-top  p-3"
+//  alt="..."
+//  />
+//  <div class="card-body">
+//  <h5 class="card-title text-truncate overflow-hidden text-nowrap">${data.data[randomNum].album.title}</h5>
+//  <p class="card-text text-truncate overflow-hidden">${data.data[randomNum].artist.name}</p>
+// </div>
+// </div>;` 
+// row.appendChild(col);
+// }
+// }
 
-let card=document.querySelector("h5")
-card.addEventListener("click",cardSearch)
-console.log(card)
+
+// document.addEventListener("click",cardSearch)
+
+
 
 // document.addEventListener("click",cardSearch)
 
@@ -106,10 +118,3 @@ formSearch.addEventListener('submit', function (e) {
   
   
   
-const sidebar = document.getElementById("side-bar");
-const middle = document.getElementById("col-middle");
-  
-const funz = function () {
-    sidebar.classList.toggle("d-md-none");
-    middle.classList.toggle("col-lg-9");
-};
