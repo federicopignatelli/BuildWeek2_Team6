b12150eb3f65064f0d84d18378636160591cf739
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-10-19 23:25:01 +0200

    md

--- js/right-sidebar.js
+++
@@ -1,8 +0,0 @@
-// const tasto = document.getElementById("icona-sidebar");
-const sidebar = document.getElementById("side-bar");
-const middle = document.getElementById("col-middle");
-
-const funz = function () {
-  sidebar.classList.toggle("d-none");
-  middle.classList.toggle("col-lg-9");
-};
