9e15a151c3f14a8370ac337226906a92ea15fe95
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-10-20 10:30:27 +0200

    md

--- js/album.js
+++ js/album.js
@@ -9,10 +9,115 @@ const peopleIconButton = document.getElementById("people-icon")
 const sidebar = document.getElementById("side-bar")
 const colMiddle = document.getElementById("col-middle")
 
+// qui mi prendo i riferimenti del dom per i tasti play e pause e progressbar
+const playBtn = document.getElementById('play')
+const pauseBtn = document.getElementById('pause')
+const audioDiv = document.getElementById('audio')
+const progressBar = document.getElementById('progressBar');
+const divPlay = document.getElementById('container')
 
+// creocostante audio con costrutto per il play
+const audio = new Audio();
+let isPlaying = false;
 
 const addressBarContent = new URLSearchParams(location.search)
 const albumId = addressBarContent.get("albumId")
+
+fetch("https://striveschool-api.herokuapp.com/api/deezer/album/" + albumId)
+
+    .then((response) => {
+        hideSpinner()
+        if (response.ok) {
+            return response.json()
+        } else {
+            throw new Error("Errore")
+        }
+    })
+    .then((obj) => {
+        console.log(obj)
+        songs(obj)
+        const start = document.querySelectorAll('.clickTrack')
+        start.forEach((start1, index) => {
+            start1.addEventListener('click', () => {
+                console.log('na merda', start1)
+                const song = obj.data[index]
+                console.log(song)
+                const divFirst = document.getElementById('brano')
+                const div = document.createElement('div')
+                div.style.display = 'flex'
+                div.style.alignItems = 'center'
+                divFirst.innerHTML = ''
+                div.innerHTML = ` <div class="col-lg-3 flex-grow-1 ps-2">
+      <img src="${song.album.cover_medium}" alt="" />
+    </div>
+    <div>
+      <h6>${song.title}</h6>
+      <p>${song.artist.name}</p>
+    </div>
+    <div>
+    <button
+      aria-checked="false"
+      data-testid="add-button"
+      class="Button-sc-1dqy6lx-0 jaXpQg control-button control-button-heart"
+      aria-label="Salva in La tua libreria"
+      data-encore-id="buttonTertiary"
+      aria-expanded="false"
+    >
+      <span
+        aria-hidden="true"
+        class="IconWrapper__Wrapper-sc-16usrgb-0 hYdsxw"
+        ><svg
+          style="fill: hsla(0, 0%, 100%, 0.7)"
+          role="img"
+          height="16"
+          width="16"
+          aria-hidden="true"
+          viewBox="0 0 16 16"
+          data-encore-id="icon"
+          class="Svg-sc-ytk21e-0 haNxPq"
+        >
+          <path
+            d="M1.69 2A4.582 4.582 0 0 1 8 2.023 4.583 4.583 0 0 1 11.88.817h.002a4.618 4.618 0 0 1 3.782 3.65v.003a4.543 4.543 0 0 1-1.011 3.84L9.35 14.629a1.765 1.765 0 0 1-2.093.464 1.762 1.762 0 0 1-.605-.463L1.348 8.309A4.582 4.582 0 0 1 1.689 2zm3.158.252A3.082 3.082 0 0 0 2.49 7.337l.005.005L7.8 13.664a.264.264 0 0 0 .311.069.262.262 0 0 0 .09-.069l5.312-6.33a3.043 3.043 0 0 0 .68-2.573 3.118 3.118 0 0 0-2.551-2.463 3.079 3.079 0 0 0-2.612.816l-.007.007a1.501 1.501 0 0 1-2.045 0l-.009-.008a3.082 3.082 0 0 0-2.121-.861z"
+          ></path></svg
+      ></span>
+    </button>
+  </div>`
+                divFirst.appendChild(div)
+
+                if (song && song.preview) {
+                    audio.src = song.preview;
+                    audio.play();
+                    playBtn.style.display = 'none'
+                    pauseBtn.style.display = 'block'
+                    isPlaying = true
+                    divPlay.classList.remove('d-none')
+                }
+
+
+            })
+        })
+        playBtn.addEventListener('click', () => {
+            if (!isPlaying) {
+                playBtn.style.display = 'none'
+                pauseBtn.style.display = 'block'
+                isPlaying = true
+                audio.play()
+            }
+        })
+        // funzione per il pause
+        pauseBtn.addEventListener("click", () => {
+            if (isPlaying) {
+                playBtn.style.display = "block";
+                pauseBtn.style.display = "none";
+                isPlaying = false;
+                audio.pause();
+            }
+        });
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
 
 const header = (obj1) => {
 
@@ -133,7 +238,7 @@ const songs = (obj1) => {
         </div>
 
         <div class="">
-        <a class="btn text-start p-0 text-light border-0" href=""><h6 class="fw-bold mb-0">${objData.title}</h6></a>
+        <a class="btn text-start p-0 text-light border-0 clickTrack text-decoration-none" href=""><h6 class="fw-bold mb-0">${objData.title}</h6></a>
         <p class="m-0 opacity-50" style="font-size:12px">${objData.artist.name}</p>
         </div>
 
@@ -152,23 +257,6 @@ const songs = (obj1) => {
 
 
 
-fetch("https://striveschool-api.herokuapp.com/api/deezer/album/" + albumId)
-
-    .then((response) => {
-        hideSpinner()
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw new Error("Errore")
-        }
-    })
-    .then((obj) => {
-        console.log(obj)
-        songs(obj)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
 
 
 
